4f859b6e80a9a0e01917d257b85c080200707ce6
Zebulun Arendsee <arendsee@iastate.edu>
2016-01-17 17:02:42 -0600

    Finish implementation of linked list
    Here we have a linked list implemented using structs. I've written
    recursive functions to perform all normal operations (find, append,
    prepend, insert, splice, print). Other than prepend, all of these are
    O(n) operations. Everywhere I pass pointers rather than pass by
    reference, which is probably an ugly choice. However, for educational
    purposes, I will keep it. Perhaps I will implement a second version with
    pass by reference.

--- data-structures/03-linked-list.c
+++ data-structures/03-linked-list.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct Node {
-    int indx;
+    size_t indx;
     int a; // Just a thing to set
     struct Node * next;
     struct Node * last;
@@ -28,49 +29,90 @@ void makeList(Node * root, size_t length){
 }
 
 // recursive print through all the nodes
-void printList(Node node){
-    printf("%d %d\n", node.indx, node.a);
-    if(node.next != NULL){
-        printList(*(node.next));
+void printList(Node * node){
+    printf("%d %d\n", node->indx, node->a);
+    if(node->next != NULL){
+        printList(node->next);
     }
 }
 
-// insert a Node BEFORE the indx, i.e. the new node will be at
-// position indx in the new list.
-void insert(Node * node, Node * newnode, size_t indx){
-    Node * n = node;
-    int iid; // insertion id
-    for(iid = 0; iid < indx; iid++){
-        if(n->next == NULL){
-            newnode->last = n;
-            newnode->next = NULL;
-            n->next = newnode;
-            return;
+// insert a Node AFTER the i, i.e. the new node will be at
+// position i+1 in the new list.
+// O(n) time complexity
+void insert(Node * node, Node * newnode, size_t i){
+    bool final = (node->next == NULL);
+    if(i == 0 || final){
+        newnode->next = node->next;
+        newnode->last = node;
+        if(!final){
+            node->next->last = newnode;
         }
-        n = n->next;
+        node->next = newnode;
     }
-    if(n != NULL){
-        newnode->last = n->last;
-        newnode->next = n;
-        n->last->next = newnode;
-        n->last = newnode;
+    else {
+        insert(node->next, newnode, --i);
     }
 }
 
-void splice(Node root, size_t indx){
-
+// Remove the node indx steps from node
+// O(n) time complexity
+void splice(Node * node, size_t indx){
+    if(indx > 0){
+        if(node->next != NULL)
+            splice(node->next, --indx);
+    } else {
+        node->last->next = node->next;
+        if(node->next != NULL)
+            node->next->last = node->last;
+    }
 }
 
-void append(Node root, Node newnode){
-
+// O(n) time complexity
+void append(Node * node, Node * newnode){
+    if(node->next == NULL){
+        node->next = newnode;
+        newnode->last = node;
+        newnode->next = NULL;
+    } else {
+        append(node->next, newnode);
+    }
 }
 
-void prepend(Node root, Node newnode){
+// constant time complexity
+void prepend(Node * oldroot, Node * newroot){
 
+    // ptr to the new first element.
+    // The root pointer is invariant, but the object changes.
+    Node * a = oldroot;
+    // ptr to the new second element
+    Node * b = newroot;
+
+    newroot->next = b;
+    newroot->last = NULL;
+    oldroot->last = a;
+    oldroot->next->last = b;
+
+    // Now I want to switch pointers
+
+    // ptr to temporary newroot
+    Node tmp = *newroot;
+    // the first element is the newroot node
+    *newroot = *oldroot;
+    // the second element is the oldroot node
+    *oldroot = tmp;
 }
 
-int find(Node root, int a){
-
+// O(n) time complexity
+// Print the indx of the first node with given a
+// If no match is found, return -1
+int find(Node * node, int a){
+    if(node->a == a){
+        return(node->indx);
+    } else if(node->next != NULL) {
+        return(find(node->next, a)); 
+    } else {
+        return(-1);
+    }
 }
 
 int main(){
@@ -78,19 +120,37 @@ int main(){
     Node m;
     Node n;
 
+    printf("Make and print list\n");
     makeList(&root, 5);
-    printList(root);
+    printList(&root);
 
-    printf("\n");
+    printf("\nInsert 999 element after 2\n");
     m.indx=2; 
     m.a = 999;
     insert(&root, &m, 2);
-    printList(root);
+    printList(&root);
 
-    printf("\n");
+    printf("\nInsert 888 element at the end\n");
     n.indx=6;
     n.a = 888;
-    insert(&root, &m, 6);
-    printList(root);
+    insert(&root, &n, 6);
+    printList(&root);
+
+    printf("\nRemove the 3rd element\n");
+    splice(&root, 3);
+    printList(&root);
+
+    printf("\nAppend element 999\n");
+    append(&root, &m);
+    printList(&root);
+
+    // This blows up into an infinite loop if you try to prepend a pointer to a Node
+    // already in the list
+    printf("\nPreprend a node, rerooting the list\n");
+    splice(&root, 6);
+    prepend(&root, &m);
+    printList(&root);
+
+    printf("\nindx of element with a=16 is %d\n", find(&root, 16));
     return(0);
 }
