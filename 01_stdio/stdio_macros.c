615f05067d54a557fc896393b7c492f9d67a13fc
Zebulun Arendsee <arendsee@iastate.edu>
2016-01-19 00:28:50 -0600

    Add stdio.h macro printout

---
+++ 01_stdio/stdio_macros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+    printf("NULL            %d\n", NULL);
+    printf("_IOFBF          %d\n", _IOFBF);
+    printf("_IOLBF          %d\n", _IOLBF);
+    printf("_IONBF          %d\n", _IONBF);
+    printf("BUFSIZ          %d\n", BUFSIZ);
+    printf("EOF             %d\n", EOF);
+    printf("FOPEN_MAX       %d\n", FOPEN_MAX);
+    printf("FILENAME_MAX    %d\n", FILENAME_MAX);
+    printf("L_tmpnam        %d\n", L_tmpnam);
+    printf("SEEK_CUR        %d\n", SEEK_CUR);
+    printf("SEEK_END        %d\n", SEEK_END);
+    printf("SEEK_SET        %d\n", SEEK_SET);
+    printf("TMP_MAX         %d\n", TMP_MAX);
+    printf("stdin           %d\n", stdin);
+    printf("stderr          %d\n", stderr);
+    printf("stdout          %d\n", stdout);
+    return(0);
+}
