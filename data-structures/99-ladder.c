f6a09a5c93fd73bfbb919dd0c9309271438ea9ee
Zebulun Arendsee <arendsee@iastate.edu>
2016-01-19 02:26:13 -0600

    Add a genomic Christmas tree data structure
    This data structure is tree augmented with doubly-linked lists joining
    certain branches. It can be searched recursively from root (genome) to
    leaf (exon). Two levels, chromosome and gene, can also be iterated
    through with LAST and NEXT links. This allows direct access to siblings.
    It also permits arrays of pointers to nodes to be easily copied for
    analysis, sorting, and whatnot outside the main data structure. Genes do
    not link across chromosomes, which prevents false inferences of
    adjacency.
    
    This tree is loaded from a file of very specific format. This
    specificity makes the program a bit brittle.

--- data-structures/99-ladder.c
+++ data-structures/99-ladder.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <string.h>
 
 #define NAME_LENGTH 20
@@ -15,77 +12,62 @@
 
 #define NLEVELS 5
 
-/*
-typedef struct Match {
-    Node * a;
-    Node * b;
-    size_t abeg;
-    size_t aend;
-    size_t bbeg;
-    size_t bend;
-    char strand;
-} Match;
 
-
-typedef struct Map {
-    Node * a;
-    Node * b;
-} Map;
-
-
-typedef struct Link {
-    size_t size;
-    Node * a;
-    Node * b;
-} Link;
-*/
 
 typedef struct Node {
     char name[NAME_LENGTH];
-    size_t type;
-    size_t beg;
-    size_t end;
+    unsigned char type;
     size_t size;
     struct Node ** children;
     struct Node * next;
     struct Node * last;
 } Node;
 
+
+
 void printNode(Node * node){
-    printf("name=%s type=%d beg=%d end=%d size=%d child=%x next=%x last=%x self=%x\n",
+    printf("%s\t\ttype=%d size=%d last=%s next=%s\n",
             node->name,
             node->type,
-            node->beg,
-            node->end,
             node->size,
-            node->children,
-            node->next,
-            node->last,
-            node);
+            node->last->name,
+            node->next->name);
 }
 
-/*
-void deleteTree(Node * node){
+
+
+void recursivePrintNode(Node * node){
+    printNode(node);
+    for(unsigned int i = 0; i < node->size; i++){
+        recursivePrintNode(node->children[i]);
+    }
+}
+
+
+
+void freeNode(Node * node){
     for(int i = 0; i < node->size; i++){
-        deleteTree(node->children[i]);
+        freeNode(node->children[i]);
     }
     free(node->children);
     free(node);
 }
-*/
 
-void deleteList(Node * node){
+
+
+void freeList(Node * node){
     if(node->next != NULL){
-        deleteList(node->next);
+        freeList(node->next);
     }
-    free(node);
+    freeNode(node);
 }
 
+
+
+// Allocate space for a new node and set defaults
 Node * newNode(){
     Node * nd = (Node *) malloc(sizeof(Node));
     nd->type = 99;
-    nd->beg = 0;
-    nd->end = 0;
     nd->size = 0;
     nd->children = NULL;
     nd->next = NULL;
@@ -93,6 +75,66 @@ Node * newNode(){
     return(nd);
 }
 
+
+
+/* Determine the last/next relationships for a tree of genome data
+ *
+ * This function assumes the GENOME -> CHR -> GENE -> MRNA -> CDS/exon
+ * structure. The actual type names are not important, since they are
+ * mapped to numbers (GENOME == 0, CDS == 4).
+*/
+void wireGenomeTree(Node * node){
+    for(unsigned int i = 0; i < node->size; i++){
+        wireGenomeTree(node->children[i]);
+    }
+    if(node->size > 0){
+        node->children[0]->last = NULL;
+        node->children[node->size-1]->next = NULL;
+    }
+    if(node->size > 1 && (node->type == GENOME || node->type == CHR)){
+        for(unsigned int i = 1; i < node->size; i++){
+            node->children[i]->last = node->children[i-1];
+            node->children[i-1]->next = node->children[i];
+        }
+    }
+}
+
+
+
+/* Build a Genomic Christmas tree
+ *
+ *
+ * This function requires a very specific input that exactly follows a specific
+ * order. It must have at least three space separated arguments: 
+ *
+ * For example:
+ *  chr     1      Chr1     1   10000
+ *  gene    2        g1  3631    5899
+ *  mRNA    3      g1m1  3631    5899
+ *  CDS     4    g1m1c1  3760    3913
+ *  CDS     4    g1m1c2  3996    4276
+ *  CDS     4    g1m1c3  4486    4605
+ *  gene    2        g2  5928    8737
+ *  ...
+ *
+ *  1: TYPE  - This is actually ignored, but must be present.
+ *  2: LEVEL - Fagin uses this number to determine hierarchy level. 0 is root,
+ *             but mmust not be included.
+ *  3: NAME  - A label with no spaces, it must be less than NAME_LENGTH
+ *             characters long. There are no other constraints (currently).
+ *             It needn't be unique and can include any non-whitespace
+ *             ASCII character.
+ *  4: BEG   - The starting position of the interval.
+ *  5: END   - The ending position of the interval.
+ *
+ *  The file may contain additional columns of data, but at present these will
+ *  be ignored.
+ *
+ *  The order is REQUIRED to be depth-first recursion order based on the TYPE
+ *  filed. Currently Fagin performs NO checking on correctness of order. It
+ *  is assumed that the input was created by an (as of yet unwritten) utility
+ *  that parsed it from a GFF file and carefully validated it.
+ */
 Node * readNodeFile(char * filename, size_t nlevels){
     FILE * fp = fopen(filename, "rb");
     char * line = NULL;
@@ -115,28 +157,25 @@ Node * readNodeFile(char * filename, size_t nlevels){
     //  2. iterate backwards, counting the children of each node, return first node
     //  3. iterate forwards, filling child arrays and trimming unnecessary links
     // NOTE: it is necessary to free this memory
-    // NOTE: this leaks
 
-    // create a flat, doubly-linked list of Node objects
+    // 1. create a flat, doubly-linked list of Node objects
     // the hierarchical level is stored in type {1, 2, ..., nlevels - 1)
     Node * lastnd = root;
     while ((read = getline(&line, &len, fp)) != EOF) {
         Node * nd = newNode();
-        sscanf(line, "%*s %d %s %d %d", &nd->type, nd->name, &nd->beg, &nd->end);
+        sscanf(line, "%*s %d %s", &nd->type, nd->name);
         nd->last = lastnd;
         lastnd->next = nd;
         lastnd = nd;
-        printNode(lastnd);
     }
     lastnd->next = NULL;
-
-    printf("----\n");
 
     size_t counts[nlevels];
     for(int i = 0; i < nlevels; i++){
         counts[i] = 0;
     }
 
+    // 2. Walk backwards and count children
     Node * node = lastnd;
     while(node != NULL){
 
@@ -148,11 +187,8 @@ Node * readNodeFile(char * filename, size_t nlevels){
             counts[node->type + 1] = 0;
         }
 
-        printNode(node);
         node = node->last; 
     }
-
-    printf("----\n");
 
     Node * parents[nlevels];
     for(int i = 0; i < nlevels; i++){
@@ -160,36 +196,57 @@ Node * readNodeFile(char * filename, size_t nlevels){
         parents[i] = NULL;
     }
 
+    // 3. Makes houses for children and assign them places
     node = root;
     parents[0] = node;
     int pid;
     while(node != NULL){
+        // This is the level of the parent (e.g. 0 is GENOME and 4 is EXON)
         pid = node->type - 1;
 
         if(node->next != NULL){
+            // The input file is required to be ordered such that this works ...
             if(node->type < node->next->type){
+                // start collecting children for this node at the appropriate level
                 parents[node->type] = node;
+                // make container for children
                 node->children = (Node**)malloc(node->size * sizeof(Node*));
+                // reset child count at this level
                 counts[node->type] = 0;
-                node->next->last = NULL;
             }
         }
 
-        // Here I seem to be writing to some weird place in memory, it is
-        // overwriting STDOUT
-        //parents[pid]->children[counts[pid]] = node;
+        // assign this child to a parent, unless it is root (pid == -1)
+        if(pid > -1)
+            parents[pid]->children[counts[pid]] = node;
+
+        // count this child (needed for correct assignment of next sibling)
         counts[node->type - 1]++;  
 
-        printNode(node);
+        // progress to next node (remember this is still a flat, doubly-linked list)
         node = node->next;
     }
 
     return(root);
 }
 
+
+
+// Load a tree and print it
 int main(int argc, char* argv[]){
+
+    if(argc < 2){
+        printf("USAGE: ./a.out <filename>\n");
+        exit(EXIT_FAILURE);
+    }
 
     Node * root = readNodeFile(argv[1], NLEVELS);
 
-    return(0);
+    wireGenomeTree(root);
+
+    recursivePrintNode(root);
+
+    freeNode(root);
+
+    exit(EXIT_SUCCESS);
 }
