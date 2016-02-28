cccfb4e479a19c045cebf278b3bfba40e5e68f9d
Zebulun Arendsee <arendsee@iastate.edu>
2016-02-27 23:52:08 -0600

    Built functional (though slow) hash function

--- data-structures/hash/hash.c
+++ data-structures/hash/hash.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define SALT 18747
+
 #define byte unsigned char
+#define uint unsigned int
+#define ushort unsigned short
 
 // Rotate one byte left
-#define rotl(x) (x) << 1 ^ (x) >> 7
+#define byte_rotl(x) (x) << 1 ^ (x) >> 7
 // Rotate one byte right
-#define rotr(x) (x) >> 1 ^ (x) << 7
+#define byte_rotr(x) (x) >> 1 ^ (x) << 7
+
+#define rotl(x,n,k) (x) << (k) ^ (x) >> ((n) - (k))
+#define rotr(x,n,k) (x) >> (k) ^ (x) << ((n) - (k))
 
 // Rotate k places on a block n bit long
 #define grotl(x,n,k) (x) << ((k) % (n)) ^ (x) >> ((n) - ((k) % (n)))
 #define grotr(x,n,k) (x) >> ((k) % (n)) ^ (x) << ((n) - ((k) % (n)))
 
+ushort swap(ushort x, size_t a, size_t b){
+    size_t size = sizeof(ushort);
+    if(a < b){
+        size_t tmp = a;
+        a = b;
+        b = tmp; 
+    }
+    if(a >= size || b >= size){
+        fprintf(stderr, "Swap positions must be between 0 and size - 1\n");
+        exit(1);
+    }
+    if(a == b)
+        return x;
+    return (x & (1 << a)) >> (a - b) |
+           (x & (1 << b)) << (a - b) | x;
+}
+
 void print_binary(void * data, size_t size){
     byte * v = (byte *) data;
-    for(int i = 0; i < size; i++){
+    for(int i = size - 1; i > -1; i--){
         for(byte mask = 128; mask != 0; mask >>= 1){
             printf("%d", v[i] & mask ? 1 : 0);
         }
@@ -32,27 +57,40 @@ byte xor_all(void * data, size_t size, byte hash){
     return(hash); 
 }
 
+ushort hash(void * key, size_t size){
+    ushort * k = (ushort *) key;
+    ushort hash = SALT;
+    size_t pad_length = size % sizeof(ushort);
+    int give_me_freedom = 0;
+    // If the input is not a multiple of the hash block size pad with 0s
+    if(pad_length != 0){
+        size += pad_length;
+        k = (ushort *)realloc(k, size * sizeof(ushort));
+        for(int i = size - pad_length; i < size; i++){
+            k[i] = 0;
+        }
+        give_me_freedom++;
+    }
+    for(int i = 0; i < size; i++){
+        hash ^= k[i] << sizeof(byte);
+        hash ^= k[i] >> sizeof(byte);
+        hash ^= k[size - i - 1];
+    }
+    if(give_me_freedom)
+        free(k);
+    return hash; 
+}
+
 int main(int argc, char * argv[]){
-    char * a = "xor this line";
-    print_binary(a, strlen(a) + 1);
-    byte hash = 0;
-    hash = xor_all(a, strlen(a), hash);
-    print_binary(&hash, 1);
-    byte b = 24;
-    for(int i = 0; i < 8; i++){
-        print_binary(&b, 1);
-        b = rotl(b);
-    }
-    for(int i = 0; i < 8; i++){
-        print_binary(&b, 1);
-        b = rotr(b);
-    }
-    printf("\n");
-    b = 24;
-    for(int i = 0; i < 64; i++){
-        print_binary(&b, 1);
-        printf("%d\n", i % 8);
-        b = grotr(b, 8, i);
+    int r;
+    int n = 10;
+    if(argc > 1)
+        srand(atoi(argv[1]));
+    if(argc > 2)
+        n = atoi(argv[2]);
+    for(int i = 0; i < n; i++){
+        r = rand();
+        printf("%d\n", hash(&r, sizeof(int)));
     }
     return 0;
 }
